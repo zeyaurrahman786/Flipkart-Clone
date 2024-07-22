f6c6d2e3d6fa1331064ef786e3ca2d1595d084cd
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-22 12:06:20 +0530

    Flipkart Clone

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,26 @@
-console.log("Hello world!");
+let mongoose = require("mongoose");
+let express = require("express");
+let app = express();
+
+app.use(express.json());
+let loginRoutes = require("./routes/login");
+app.use(express.urlencoded({ extended: true }));
+let userRoutes = require("./routes/user");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/flipkart")
+  .then(() => {
+    console.log("Database Coneected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api", userRoutes);
+app.use("/api", loginRoutes);
+
+// localhost:5000/api/users
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
