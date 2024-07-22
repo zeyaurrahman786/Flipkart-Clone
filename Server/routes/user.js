3d453b86af79b6b0e3718e0bd89ff156058c5803
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-22 12:06:05 +0530

    Flipkart Clone

---
+++ Server/routes/user.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+let bcrypt = require("bcrypt");
+
+
+router.post("/users", async (req, res) => {
+  const user = req.body;
+  console.log(req.body, "Zeyaur");
+  const Email = await User.findOne({ email: user.email });
+  if (Email) {
+    res.send("User is already registered in our Database");
+  } else {
+    // console.log(req.body.passWord);
+    user.passWord = await bcrypt.hash(req.body.passWord, 10);
+    const dbUser = new User({
+      name: user.name,
+      email: user.email,
+      passWord: user.passWord,
+    });
+    await dbUser.save();
+    res.send({ messge: "Done" });
+  }
+});
+module.exports = router;
